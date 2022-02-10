3d2de9706677d38eef0d13ef63dd88a996fb4e50
mikaelpardes <mikaelpardes@gmail.com>
2022-02-10 15:17:16 +0200

    create song class

--- music/song.py
+++ music/song.py
@@ -1,5 +1,20 @@
-class Songs:
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
+import json
 
+
+class Song:
+    def __init__(self, songId, songName, songPopularity):
+        self.songId = songId
+        self.songName = songName
+        self.songPopularity = songPopularity
+
+    def __str__(self):
+        return "songId: {0}, songName: {1}, songPopularity:{2}".format(self.songId, self.songName,
+
+                                                                       self.songPopularity)
+
+
+def songDetails(songPath):
+    with open(songPath) as song:
+        json_song = json.load(song)
+        song = Song(json_song["track"]["id"], json_song["track"]["name"], json_song["track"]["popularity"])
+    return (song)
